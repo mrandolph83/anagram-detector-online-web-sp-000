c2ab4ba9372569c8bcdca70d6ff75dd523c2287b
Matt Smith <mrandolph83@gmail.com>
2020-11-02 16:26:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,8 @@ class Anagram
     @word = word
   end
 
-  def match(word_array)
-    word_array.select do |word|
+  def match(strings)
+    strings.select do |word|
       word.split("").sort == @word.split("").sort
     end
   end
