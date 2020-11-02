94674d8e08785646ce334a5e3b75cfbc2118a66c
Matt Smith <mrandolph83@gmail.com>
2020-11-02 15:59:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Words
+
+  attr_accessor :words
+
+  def initialize(words)
+# split into an array ("")
+  @words = %w(words)
+  
+  
+  end
+  
+
+end# Your code goes here!
